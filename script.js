3d0da5b5f06e97e57b40aff4528e740e9fb56a2a
Peterson Adler de Medeiros Araújo <104148184+PetersonAdler@users.noreply.github.com>
2022-04-26 21:32:10 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+alert ('Oi bê! Tudo bem?! Espero que sim!!')
+let nome = prompt ('Sei que é você, mas só para ter certeza, você é a peppona né? (sim ou não)')
+if(nome.toLowerCase() == 'sim'){
+    alert ('Pronto, agora que teho certeza que é você, pode ler o textinho que fiz para ti! 😍')
+}else{
+    alert('você é a peppona sim, aceite, agora leia o texto! 😅')
+}
